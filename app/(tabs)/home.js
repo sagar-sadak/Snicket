f207a15e516f97007c2c7b79bdb686cdcae32067
aditi <me20btech11001@iith.ac.in>
2025-03-02 12:08:30 -0500

    Added dummy books on home page

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -1,10 +1,18 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { useRouter } from 'expo-router';
 import { getAuth, signOut } from "firebase/auth";
+
+const books = [
+  { id: '1', title: 'Book 1', user: 'Sarika'},
+  { id: '2', title: 'Book 2', user: 'Nitin' },
+  { id: '3', title: 'Book 3', user: 'Sagar'},
+];
+
 
 export default function HomeScreen() {
   const router = useRouter();
   const auth = getAuth();
+
   const exit = () => {
     signOut(auth).then(() => {
       console.log("Logged out");
@@ -19,6 +27,20 @@ export default function HomeScreen() {
       <TouchableOpacity style={styles.button} onPress={exit}>
         <Text style={styles.buttonText}>Sign Out</Text>
       </TouchableOpacity>
+
+      <FlatList
+        data={books}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.bookItem}>
+            <View style={styles.bookInfo}>
+              <Text style={styles.bookTitle}>{item.title}</Text>
+              <Text style={styles.bookUser}>Listed by {item.user}</Text>
+            </View>
+          </View>
+        )}
+      />
+
     </View>
   );
 }
@@ -48,5 +70,28 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '500',
     textAlign: 'center',
-  }
+  },
+  bookItem: {
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    padding: 12,
+    borderRadius: 10,
+    marginVertical: 8,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  bookInfo: {
+    flex: 1,
+  },
+  bookTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  bookUser: {
+    fontSize: 14,
+    color: '#666',
+  },
 });
